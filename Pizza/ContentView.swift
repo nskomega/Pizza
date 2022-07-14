2ad5d9b0358a1b42d4dd77acd927fbd299c103fb
Mikhail Danilov <2588000nsk@gmail.com>
2022-07-14 08:59:58 +0300

    add autorization

--- Pizza/ContentView.swift
+++ Pizza/ContentView.swift
@@ -8,9 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var email = ""
+    @State private var password = ""
+
     var body: some View {
-        Text("Hello, world!")
+
+        VStack(spacing: 15) {
+            Text("Авторизация")
+                .padding()
+                .padding(.horizontal, 30)
+                .font(.title2.bold())
+                .background(Color("alphaWhite"))
+                .cornerRadius(30)
+            VStack {
+                TextField("Введите Email", text: $email)
+                    .padding()
+                    .background(Color("alphaWhite"))
+                    .cornerRadius(12)
+                    .padding(10)
+                    .padding(.horizontal, 12)
+
+                SecureField("Введите пароль", text: $password)
+                    .padding()
+                    .background(Color("alphaWhite"))
+                    .cornerRadius(12)
+                    .padding(10)
+                    .padding(.horizontal, 12)
+
+                Button {
+                    print("Авторизация")
+                } label: {
+                    Text("Войти")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(LinearGradient(colors: [Color("orange"), Color("yellow")], startPoint: .trailing, endPoint: .leading))
+                        .cornerRadius(12)
+                        .padding(10)
+                        .padding(.horizontal, 12)
+                        .font(.title3.bold())
+                        .foregroundColor(.black)
+                }
+                Button {
+                    print("Регистрация")
+                } label: {
+                    Text("Регистрация")
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .cornerRadius(12)
+                        .padding(10)
+                        .padding(.horizontal, 12)
+                        .font(.title3.bold())
+                        .foregroundColor(Color("brown"))
+                }
+            }
             .padding()
+            .background(Color("alphaWhite"))
+            .cornerRadius(26)
+            .padding(30)
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Image("background").ignoresSafeArea())
     }
 }
 
