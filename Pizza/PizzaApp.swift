1a7b3fb39430a681d74638872ebf2269d248066d
Mikhail Danilov <2588000nsk@gmail.com>
2022-07-14 05:31:36 +0300

    initial commit

---
+++ Pizza/PizzaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaApp.swift
+//  Pizza
+//
+//  Created by Mikhail Danilov on 14.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct PizzaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
